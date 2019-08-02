749c81cb3d0b93802737704f3c561bbde243b41e
Sarah Cullen <scwebd@gmail.com>
2019-08-02 00:35:13 -0600

    Cleaning up routing behavior

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,23 @@
 module.exports = {
     signup: function (req, res) {
+        // if user is logged in, redirect to /dashboard
+        if (req.user) {
+            return res.redirect("/dashboard");
+        }
         var error = req.flash("error")[0];
         res.render("signup", { error: error });
     },
     login: function (req, res) {
+        // if user is logged in, redirect to /dashboard
+        if (req.user) {
+            return res.redirect("/dashboard");
+        }
         var error = req.flash("error")[0];
         res.render("login", { error: error });
-    }, 
+    },
     dashboard: function (req, res) {
         res.render("dashboard");
-    }, 
+    },
     logout: function (req, res) {
         req.session.destroy(function (err) {
             res.redirect("/");
