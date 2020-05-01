4553b530c619fe745b2e7e356da2fc9e551f0aba
Sarah Cullen <scwebd@gmail.com>
2020-04-30 22:27:24 -0600

    Small style, functionality, and dependency tweaks

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -28,6 +28,10 @@ module.exports = function (passport, user) {
         passwordField: "password",
         passReqToCallback: true
     }, function (req, email, password, done) {
+        if (password.length < 8 || password.toUpperCase() === password || password.toLowerCase() === password) {
+            return done(null, false, { message: "Passwords must be at least 8 characters and must contain uppercase and lowercase letters"});
+        }
+
         var generateHash = function (password) {
             return bCrypt.hashSync(password, bCrypt.genSaltSync(process.env.SALT_NUM), null);
         };
